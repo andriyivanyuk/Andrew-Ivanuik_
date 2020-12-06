84ddbe5a0615ca33615706143a8a7a8f6b27e458
Andriy Ivanyuk <anymacstore@MacBook-Pro-AnyMac.local>
2020-12-06 23:32:06 +0200

    app initiated

---
+++ main.js
@@ -0,0 +1,9 @@
+//Variables
+const favListDOM = document.querySelector('.favourite-list');
+const favouriteItems = document.querySelector('.favourite-items');
+const cardContent = document.querySelector('.card');
+const cardDOM = document.querySelector('.cards-wrapper');
+
+let favouriteList = [];
+
+
